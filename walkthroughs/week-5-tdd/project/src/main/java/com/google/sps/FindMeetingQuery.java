836a9934ffa40af6db016ed9b54e32eb3af83da4
Laura O'Donnell <laura.od@live.com>
2020-08-06 16:53:34 +0000

    Remove duplicate code

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -23,12 +23,19 @@ public final class FindMeetingQuery {
   //needs to return a collection of timeranges that suit all attending parties
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     //Run One: Attempt to accomodate all parties
-    //Step One: go through each event, find out which events request participants are in, get timerange
-    ArrayList<TimeRange> conflictingEventTimes = new ArrayList<>();
     ArrayList<String> attendees = new ArrayList<>();
     attendees.addAll(request.getAttendees());
     attendees.addAll(request.getOptionalAttendees());
-    
+    Collection<TimeRange> ranges = checkTimes(events, request, attendees);
+
+    if(ranges.isEmpty()){
+      ranges = checkTimes(events, request, request.getAttendees());
+    }        
+    return ranges;
+  }
+
+  public Collection<TimeRange> checkTimes(Collection<Event> events, MeetingRequest request, Collection<String> attendees) {
+      ArrayList<TimeRange> conflictingEventTimes = new ArrayList<>();
     for(Event event: events) {
       for(String attendee: attendees) {
         if (event.getAttendees().contains(attendee)){
@@ -72,54 +79,6 @@ public final class FindMeetingQuery {
         iterator.remove();
       }
     }
-
-    //If the resulting array is empty, repeat without optional attendees
-    if(ranges.isEmpty()){
-      conflictingEventTimes = new ArrayList<>();
-      for(Event event: events) {
-        for(String attendee: request.getAttendees()) {
-          if (event.getAttendees().contains(attendee)){
-            conflictingEventTimes.add(event.getWhen());
-            break;
-          }
-        }
-      }
-      Collections.sort(conflictingEventTimes,TimeRange.ORDER_BY_START);
-
-      //Step Two: starting with the whole day, break the time range into smaller ranges each time there is a conflict
-      ranges = new ArrayList<>();
-      ranges.add(TimeRange.WHOLE_DAY); 
-      for(TimeRange eventTime: conflictingEventTimes){
-        for(Iterator<TimeRange> iterator = ranges.iterator(); iterator.hasNext();){
-          TimeRange timerange = iterator.next();
-          if(timerange.contains(eventTime)) {
-            //Break containing TimeRange into 2 TimeRanges, before and after the conflict, then remove original
-            ranges.add(TimeRange.fromStartEnd(timerange.start(), eventTime.start(), false)); //new range before event
-            ranges.add(TimeRange.fromStartEnd(eventTime.end(), timerange.end(), false)); //new range after event
-            ranges.remove(timerange);
-            break;
-          }
-          else if(timerange.overlaps(eventTime)) {
-            if(timerange.start() < eventTime.start()) {
-              ranges.add(TimeRange.fromStartEnd(timerange.start(), eventTime.start(), false));
-            }
-            else {
-              ranges.add(TimeRange.fromStartEnd(eventTime.end(), timerange.end(), false));
-            }  
-            ranges.remove(timerange);
-            break;       
-          }        
-        }
-      }
-      //Step 3: Make sure all timeranges are long enough for the request
-      for(Iterator<TimeRange> iterator = ranges.iterator(); iterator.hasNext();) {
-        TimeRange timerange = iterator.next();
-        if (timerange.duration() < request.getDuration()){
-          iterator.remove();
-        }
-      }
-    }
     return ranges;
   }
-
 }
